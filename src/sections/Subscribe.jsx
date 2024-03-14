ed1fc9670716c481983801440c0a07b5f7f9b9e4
Phil <philwekullo@gmail.com>
2024-03-14 07:09:03 +0300

    Complete site

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Button from '../components/Button'
 
 const Subscribe = () => {
   return (
-    <div>
-      SUBSCRIBE
-    </div>
+    <section className='max-container flex justify-between items-center max-lg:flex-col gap-10' id='contact-us'>
+      <h3 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>Sign Up For <span className='text-coral-red'>Updates</span> & Newsletter</h3>
+      <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
+        <input type="text" placeholder='Subscribe@nike.com' id="" className='input' />
+        <div className='flex max-sm:w-full max-sm:justify-end items-center'>
+          <Button label='Sign Up' fullWidth/>
+        </div>
+      </div>
+    </section>
   )
 }
 
